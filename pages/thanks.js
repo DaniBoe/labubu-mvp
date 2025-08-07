e796f8e9aa1b36a5a94758a275ac021da2e32061
Daniel <daniel.boettcher89@gmail.com>
2025-08-07 07:45:12 +0200

    Email form

--- pages/thanks.js
+++ pages/thanks.js
@@ -5,7 +5,7 @@ export default function Thanks() {
     <Layout>
       <div style={{ textAlign: 'center', marginTop: '5rem' }}>
         <h1>Thank you for signing up! 🎉</h1>
-        <p>We appreciate your interest. We'll notify you as soon as Labubu tools launch.</p>
+        <p>We appreciate your interest. We will notify you as soon as Labubu tools launch.</p>
       </div>
     </Layout>
   );
